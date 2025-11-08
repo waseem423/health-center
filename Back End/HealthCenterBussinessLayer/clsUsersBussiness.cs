664390fd69ddf55a05a9f53d7934ccccc9888756
waseem423 <w3290586@gmail.com>
2025-11-08 18:20:58 +0300

    Commit

---
+++ Back End/HealthCenterBussinessLayer/clsUsersBussiness.cs
@@ -0,0 +1,91 @@
+﻿using HealthCenterDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCenterBussinessLayer
+{
+
+    public class clsUsersBussiness
+    {
+        public enum enMode { AddNew=0,Update=1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int UserID { set; get; }
+
+        public int PersonID { set; get; }
+
+        public string Username { set; get; }
+
+        public string Password { set; get; }
+
+        public string Role { set; get; }
+
+        public bool IsActive { set; get; }
+
+        public UserDTO UDTO
+        {
+            get { return (new UserDTO(this.UserID, this.PersonID, this.Username, this.Password, this.Role, this.IsActive)); }
+        }
+
+        public clsUsersBussiness(UserDTO UDTO,enMode cMode=enMode.AddNew)
+        {
+            this.UserID = UDTO.UserID;
+            this.PersonID = UDTO.PersonID;
+            this.Username = UDTO.Username;
+            this.Password = UDTO.Password;
+            this.Role = UDTO.Role;
+            this.IsActive = UDTO.IsActive;
+        }
+
+        public static clsUsersBussiness Find(string Username,string Password)
+        {
+            UserDTO UDTO = clsUsersDataAccess.GetUserByUsernameAndPassword(Username, Password);
+
+            if(UDTO!=null)
+            {
+                return new clsUsersBussiness(UDTO, enMode.Update);
+            }
+
+            return null;
+        }
+
+        private bool _AddNewUser()
+        {
+            this.UserID = clsUsersDataAccess.AddNewUser(UDTO);
+            return (this.UserID != -1);
+        }
+
+        private bool _UpdateUser()
+        {
+            return false;
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    {
+                        if (_AddNewUser())
+                        {
+                            this.Mode = enMode.Update;
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                case enMode.Update:
+                    {
+                        return _UpdateUser();
+                    }
+            }
+            return false;
+        }
+
+    }
+}
---
+++ Back End/HealthCenter_API/Controllers/UsersApi.cs
@@ -0,0 +1,72 @@
+﻿using HealthCenterBussinessLayer;
+using HealthCenterDataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthCenter_API.Controllers
+{
+    [Route("api/Users")]
+    [ApiController]
+    public class UsersApi : ControllerBase
+    {
+
+
+        [HttpPost(Name = "AddUser")]
+        public bool AddnewUserFromRegister(UserDTOForRegister newUserDTOForRegister)
+        {
+            if (newUserDTOForRegister == null || string.IsNullOrEmpty(newUserDTOForRegister.FirstName) 
+                || string.IsNullOrEmpty(newUserDTOForRegister.NationalNo) || string.IsNullOrEmpty(newUserDTOForRegister.Username)
+                || string.IsNullOrEmpty(newUserDTOForRegister.Password)
+                || string.IsNullOrEmpty(newUserDTOForRegister.Phone)|| string.IsNullOrEmpty(newUserDTOForRegister.Gender))
+            {
+                return false;
+            }
+
+            clsPeopleBussiness Person = new clsPeopleBussiness(new PeopleDTO(-1,newUserDTOForRegister.NationalNo,
+                newUserDTOForRegister.FirstName,newUserDTOForRegister.LastName,newUserDTOForRegister.Phone, newUserDTOForRegister.Email,
+                newUserDTOForRegister.Address, newUserDTOForRegister.DateOfBirth, newUserDTOForRegister.Gender, newUserDTOForRegister.ImagePath));
+
+            if(Person.Save())
+            {
+                clsUsersBussiness User = new clsUsersBussiness(new UserDTO(-1, Person.PersonID, newUserDTOForRegister.Username, newUserDTOForRegister.Password,
+                    newUserDTOForRegister.Role));
+
+                if(User.Save())
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
+        }
+
+
+        // this for Login
+        [HttpGet("{Username}/{Password}", Name = "GetUserByUsernameandpassword")]
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<UserDTO> GetUserByUsernameandPassword(string Username,string Password)
+        {
+            if (string.IsNullOrEmpty(Username)|| string.IsNullOrEmpty(Password))
+            {
+                return BadRequest($"Not accepted empty Username,Password");
+
+            }
+
+            var User = clsUsersBussiness.Find(Username,Password);
+
+            if (User == null)
+                return NotFound($"User with Username {Username} and Password {Password} Is Not Found.");
+
+            UserDTO UDTO = User.UDTO;
+
+            return Ok(UDTO);
+        }
+
+        
+
+    }
+}
